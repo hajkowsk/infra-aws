46401cbb3c5ba785bb7c19c795d110386b180c3e
hajkowsk <piotr.hajkowski@gmail.com>
2020-10-18 00:23:47 +0200

    1.3 dynamic resources

--- main.tf
+++ main.tf
@@ -3,12 +3,20 @@ provider "aws" {        #A
   region  = "us-west-2" #B
 }
 
+data "aws_ami" "ubuntu" { #A
+  most_recent = true
+  filter { #B
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["099720109477"] #C
+}
 
-resource "aws_instance" "helloworld" {    #A
-  ami           = "ami-09dd2e08d601bff67" #B
-  instance_type = "t2.micro"              #B
-  tags = {                                #B
-    Name = "HelloWorld"                   #B
-  }                                       #B
+resource "aws_instance" "helloworld" {   #A
+  ami           = data.aws_ami.ubuntu.id #B
+  instance_type = "t2.micro"             #B
+  tags = {                               #B
+    Name = "HelloWorld"                  #B
+  }                                      #B
 }
 
